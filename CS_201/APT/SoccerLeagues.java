44d91609445e41c861d54343e0449844fa0120bf
Pierson Jones <piersonjones704@gmail.com>
2026-09-25 18:36:37 -0400

    Successfully solved this APT according to the prompt requirements.

---
+++ CS_201/APT/SoccerLeagues.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class SoccerLeagues {
+    public int[] points(String[] matches) {
+        // you write code here
+        int size = matches.length;
+        ArrayList<Integer> team_scores = new ArrayList<>(Collections.nCopies(size, 0));
+        // int [] team_scores = new int[300];
+        // int [] team_scores = new int[size];
+        int team_num = 0;
+        for (String i : matches) {
+            // int team_score = 0;
+            for (int j = 0; j < i.length(); j++) {
+                if (j == team_num) {} 
+                else {
+                    // if team i won then team j lost
+                    if (i.charAt(j) == 'W') {
+                        team_scores.set(team_num, team_scores.get(team_num) + 3);
+                    }
+                    // if team i lost then team j won
+                    if (i.charAt(j) == 'L') {
+                        team_scores.set(j, team_scores.get(j) + 3);
+                    }
+                    // if team i got a draw, then team j got a draw
+                    if (i.charAt(j) == 'D') {
+                        team_scores.set(team_num, team_scores.get(team_num) + 1);
+                        team_scores.set(j, team_scores.get(j) + 1);
+                    }
+                }
+            }
+            team_num += 1;
+        }
+    int [] array_t_scores = team_scores.stream().mapToInt(Integer::intValue).toArray();
+    return array_t_scores;
+    }
+    public static void main(String[] args) {
+        System.err.println("hello");
+    }
+}
+
+
